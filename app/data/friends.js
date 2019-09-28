315bfe925c13224448ab35edfe3133df718f895e
Niles Dyer-Hunter <NXDH927@outlook.com>
2019-09-27 21:11:56 -0600

    Fin

---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+var friends = [
+    
+    {
+        name:"Niles Hunter",
+        photo: "",
+        scores:[5,2,3,5,5,5,5,3,1,3]
+    },
+
+    {
+        name:"Steve Rogers",
+        photo: "",
+        scores: [5,3,3,1,5,2,5,1,5,5]
+    },
+
+    {
+        name:"Tony Stark",
+        photo: "",
+        scores: [5,5,3,1,5,1,5,1,2,5]
+    },
+
+    {
+        name:"Aragorn",
+        photo: "",
+        scores: [5,5,5,1,5,8,5,1,5,1]
+    },
+
+    {
+        name:"Thanos",
+        photo: "",
+        scores: [1,1,1,1,1,1,5,1,1,1]
+    },
+
+    {
+        name:"Saron",
+        photo:"",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    }
+
+]
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,56 @@
+var friendsData = require('../data/friends.js');
+
+module.exports = function (app){
+
+app.get("/api/tables", function(req, res) {
+    res.json(friendsData);
+  });
+
+  app.post("/app/data/friends",function(req, res){
+
+    friendsData.push(req.body);
+
+    var friendsList = friendsData.length - 1;
+    var differenceList = [];
+    var differenceNum = 0;
+
+    for (var i=0; i<friendsList; i++){
+
+      var score = friendsData[i].scores;
+      var currentScore = req.body.scores;
+
+      // for (var x=0; x<score.length; x++){
+        function myFunc(total, num) {
+          return parseInt(total) - parseInt(num);
+        }
+
+        var friendScore = score.reduce(myFunc);
+        var userScore = currentScore.reduce(myFunc);
+
+
+
+        if (friendScore != userScore ){
+          differenceNum = 0;
+          differenceNum += Math.abs(friendScore - userScore);        
+        }
+
+        else{
+          differenceNum += 0;
+        };
+
+        differenceList.push(differenceNum);
+
+      // }
+
+    };
+
+    var lowestNum = Math.min.apply(Math, differenceList);
+    var index = differenceList.indexOf(lowestNum);
+    console.log(friendsData[index]);
+    
+    res.json(friendsData[index]);
+
+  })
+
+
+};
